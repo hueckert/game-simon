9e50b1db6a1b9a26a8d3577d72d535f19c9df623
howard <howard.ueckert@protonmail.com>
2024-11-04 22:27:45 -0600

    sudocode and logic of game on js

---
+++ game.js
@@ -0,0 +1,28 @@
+// variables
+//----------------------
+
+// functions
+//-------------------
+//animation of button when pressed "linked" on css
+
+//make sound when pressed
+
+// create pattern
+
+//event handlers
+//---------------------
+
+//press any key to start
+
+//mouse click for userclinckedPattern
+
+// animation and sound when button is clicked
+
+// to check if the userClickedPattern is equal to gamePattern
+
+// if true, run code to go to next level
+
+// if there is a difference between userClickedPattern and gamePattern
+
+//gameOver and reset game
+
