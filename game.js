b7693b7bf538025f67cab0d24083ba5862622f2f
howard <howard.ueckert@protonmail.com>
2024-11-07 21:05:22 -0600

    fix html and add to css

--- game.js
+++ game.js
@@ -1,3 +1,1 @@
-// I AM BLOWING UP MY CODE AND STARTING ALL OVER
-// I noticed I didn't think I was meeting the rubric starting all over. 
-//i didn't think i was meeting the rubric for a good grade, plus I was using some outdate code I learned before taking this class, and I didn't think it was going to work"
+// const and veriables 
