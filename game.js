1f5599ffece2824b22cfeea2794e30ddf9bcddb4
howard <howard.ueckert@protonmail.com>
2024-11-07 21:13:27 -0600

    add veriables

--- game.js
+++ game.js
@@ -1,1 +1,12 @@
-// const and veriables 
+// const and variables 
+// -----------------------------------------------
+
+const colors = ['blue', 'red', 'yellow', 'green'];
+let gamePattern = [];
+let userClickedPattern = [];
+let level = 0;
+let start = false;
+
+// -----------------------------------------------
+// FUNCTIONS
+// -----------------------------------------------
