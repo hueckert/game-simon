11909254fcf26b6587c4e4bdeee3db7a3334760f
howard <howard.ueckert@protonmail.com>
2024-11-07 21:54:19 -0600

    set pattern

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-// const and variables 
-// -----------------------------------------------
+//variables and const
+// -------------------------------------------
 
 const colors = ['blue', 'red', 'yellow', 'green'];
 let gamePattern = [];
@@ -17,3 +17,18 @@ function render(message) {
   header.textContent = message || `Level ${level}`;
 }
 
+// Generate next pattern
+function nextSequence() {
+  const randomNumber = Math.floor(Math.random() * colors.length);
+  const randomColor = colors[randomNumber];
+  gamePattern.push(randomColor);
+  render(`Level ${level}`);  // Update header with current level
+  // console.log(nextSequence)
+}
+
+
+
+
+
+// // Initial render to set header text
+// render("Press any key to start");
