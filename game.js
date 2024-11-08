41674e5175c9c3beb8b88fe457c374895ee6fdb6
howard <howard.ueckert@protonmail.com>
2024-11-07 22:17:49 -0600

    animate button

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
 //variables and const
 // -------------------------------------------
 
-const colors = ['blue', 'red', 'yellow', 'green'];
-let gamePattern = [];
-let userClickedPattern = [];
+const colors = ['blue', 'red', 'yellow', 'green']
+let gamePattern = []
+let userClickedPattern = []
 let level = 0;
 let start = false;
 
@@ -11,8 +11,8 @@ let start = false;
 // FUNCTIONS
 // -----------------------------------------------
 function init() {
-  gamePattern = [];
-  userClickedPattern = [];
+  gamePattern = []
+  userClickedPattern = []
   level = 0;
   start = false;
 
@@ -23,17 +23,27 @@ function init() {
 
 // Render function to update the game header
 function render(message) {
-  const header = document.querySelector(".header");
-  header.textContent = message || `Level ${level}`;
+  const header = document.querySelector(".header")
+  header.textContent = message || `Level ${level}`
 }
 
 // Generate next pattern
 function nextSequence() {
-  const randomNumber = Math.floor(Math.random() * colors.length);
-  const randomColor = colors[randomNumber];
-  gamePattern.push(randomColor);
-  render(`Level ${level}`);  // Update header with current level
+  const randomNumber = Math.floor(Math.random() * colors.length)
+  const randomColor = colors[randomNumber]
+  gamePattern.push(randomColor)
+  render(`Level ${level}`) // Update header with current level
   console.log(nextSequence)
+}
+
+//animation of button
+function animatePress(color) {
+  const button = document.getElementById(color)
+  button.classList.add("pressed")
+
+  setTimeout(() => {
+    button.classList.remove("pressed")
+  }, 150)
 }
 
 
